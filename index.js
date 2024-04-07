d749987f21570964f6eb8e59db708fe2c78f1f24
Jayesh Jain <103871719+Jayesh-JainX@users.noreply.github.com>
2024-04-07 14:53:49 +0530

    ST

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-// Required modules
-const express = require("express");
-const { exec } = require("child_process");
+const { MLabSpeedTest } = require("mlab-speed-test");
 
-// Express app
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Speed test endpoint
-app.get("/speedtest", (req, res) => {
-  // Execute speed test command
-  exec("python utils/speedtest.py --simple", (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    // Parse speed test results
-    const results = {};
-    stdout.split("\n").forEach((line) => {
-      const parts = line.split(":");
-      if (parts.length === 2) {
-        results[parts[0].trim()] = parts[1].trim();
-      }
-    });
-
-    // Send speed test results to client
-    res.json(results);
-  });
+const speedTest = new MLabSpeedTest();
+speedTest.on("server-chosen", (serverInfo) => {
+  console.log(serverInfo.location);
+});
+speedTest.on("download-complete", (downloadData) => {
+  const downloadSpeed = downloadData.LastClientMeasurement
+    ? downloadData.LastClientMeasurement.MeanClientMbps.toFixed(2)
+    : "0";
+  console.log(downloadSpeed + "Mb/s");
+});
+speedTest.on("upload-complete", (uploadData) => {
+  const uploadSpeed = uploadData.LastServerMeasurement
+    ? (
+        (uploadData.LastServerMeasurement.TCPInfo.BytesReceived /
+          uploadData.LastServerMeasurement.TCPInfo.ElapsedTime) *
+        8
+      ).toFixed(2)
+    : "0";
+  console.log(uploadSpeed + "Mb/s");
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+speedTest.run();
